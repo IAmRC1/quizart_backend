9ba7ca132381ba58b054d284bc5288235611bb8a
Rishabh Singh <rishabhcena1@gmail.com>
2021-11-23 03:02:46 +0530

    refining code

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
+const session = require('express-session');
+const logger = require('morgan');
+const birds = require('./birds')
+
 const port = 8081
+
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+
+
+// Middlewares
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret'
+}));
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.use('/birds', birds)
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
 
 app.listen(port, () => {
